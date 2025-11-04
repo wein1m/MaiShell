0926f97d7a67f387c5f777cee5f719613e5fc541
wein1m <weynn.sz@gmail.com>
2025-11-04 23:08:20 +0800

    Refactor: replace if-else logic with struct table for clarity

--- shell.c
+++ shell.c
@@ -5,6 +5,22 @@
 
 #define LINEBUF 1024
 
+struct Command {
+  char *name;
+  void (*func)(void);
+};
+
+void cmd_exit() {
+  printf("baii baii~🐇\n");
+  exit(0);
+}
+
+void cmd_clear() { system("cls"); }
+
+void cmd_hello() {
+  printf("HEHEEE~ HELLOOWWW THEREEE MY LITTE BUNNBUNNNN🐰\n");
+}
+
 void trim(char *s) {
   // change '\n' into '\0' in the end of line so it's easier to handle
   for (int i = 0; i < strlen(s); i++) {
@@ -29,28 +45,34 @@ void trim(char *s) {
   }
 }
 
-void handleCommand(char *cmd) {
+void handleCommand(char *cmd, struct Command *commands, size_t len) {
   if (!cmd) {
     return;
   } else if (strcmp(cmd, "") == 0) {
     return;
   }
 
-  if (strcmp(cmd, "exit") == 0) {
-    printf("baii baii~🐇");
-    exit(0);
-  } else if (strcmp(cmd, "clear") == 0) {
-    system("cls");
-  } else if (strcmp(cmd, "hello") == 0) {
-    printf("HEHEEE~ HELLOOWWW THEREEE MY LITTE BUNNBUNNNN🐰\n");
-  } else {
+  int found = 0;
+  for (struct Command *p = commands; p < commands + len; p++) {
+    if (strcmp(cmd, p->name) == 0) {
+      p->func();
+      found = 1;
+      break;
+    }
+  }
+
+  if (!found)
     printf("heh~ that '%s' isn't inside my dict, bunny. try to use '/help'\n",
            cmd);
-  }
 }
 
 int main() {
   char line[LINEBUF];
+  struct Command commands[] = {
+      {"exit", cmd_exit},
+      {"clear", cmd_clear},
+      {"hello", cmd_hello},
+  };
 
   while (1) {
     printf("MaiShell ~> ");
@@ -58,7 +80,7 @@ int main() {
     if (fgets(line, sizeof(line), stdin)) {
       trim(line);
       // printf("hehe~ '%s'??🐰\n", line);
-      handleCommand(line);
+      handleCommand(line, commands, (sizeof(commands) / sizeof(commands[0])));
     }
   }
 }
